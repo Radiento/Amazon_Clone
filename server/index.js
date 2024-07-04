41ee3ed9a69221b8289cc4d8e0de28a510e0dfcb
Radiento <im.deepakjakhar@gmail.com>
2024-07-05 00:27:59 +0530

    created a new flutter project

---
+++ server/index.js
@@ -0,0 +1,34 @@
+//Imports From Packages
+const express = require('express');
+const mongoose = require('mongoose');
+
+//Imports from other files
+const authRouter = require('./routes/auth.js');
+const adminRouter = require('./routes/admin.js');
+const productRouter = require('./routes/product.js');
+const userRouter = require('./routes/user.js');
+
+
+//INIT
+const PORT = process.env.PORT || 3000;
+const app = express();  
+const DB = 'mongodb+srv://deepak:Deepak%2312@cluster0.l0kpaiq.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0';
+
+
+//Middleware
+app.use(express.json());
+app.use(authRouter);
+app.use(adminRouter);
+app.use(productRouter);
+app.use(userRouter);
+
+//Connections
+mongoose.connect(DB).then(()=>{
+    console.log('Connection Successful');
+}).catch(e => {
+    console.log(e);
+});
+
+app.listen(PORT,"0.0.0.0", () => {
+    console.log(`connected to port ${PORT}`)
+});
